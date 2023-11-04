31736325c4ecaaf07aefcb38157e3a7310230a33
RickaPrincy <rckprincy@gmail.com>
2023-11-04 10:25:23 +0300

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Project template with cmake and C++
---
+++ include/Sdlk/Sdlk.hpp
@@ -0,0 +1,10 @@
+#ifndef __SDLK__
+#define __SDLK__
+
+    #include <string>
+    
+    namespace Sdlk{
+        int add(int, int);
+        int subtract(int, int);
+    }
+#endif
