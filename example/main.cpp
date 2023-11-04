da34220907d4feabb69e548e8657c385159d7fef
RickaPrincy <rckprincy@gmail.com>
2023-11-04 13:01:23 +0300

    feat: Versioning the project

--- example/main.cpp
+++ example/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <myProject/myProject.hpp>
+#include <myProject/myProjectConfig.hpp>
 
 int main(int argc, char const *argv[]){
+
+    std::cout << "We use myProject version : " << myProject_VERSION_MAJOR << "." << myProject_VERSION_MINOR << std::endl;
+
     int result = MyProject::add(5,5);
     std::cout << "5 + 5 = " << result << std::endl; 
     
